14a600f78655ac483e76ea06834425d88eb27648
Andres <andresrosalespinzon@gmail.com>
2024-02-26 10:25:14 +0100

    test: update tests

--- src/app/shared/delete-dialog/delete-dialog.component.spec.ts
+++ src/app/shared/delete-dialog/delete-dialog.component.spec.ts
@@ -1,15 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DeleteDialogComponent } from './delete-dialog.component';
 
 describe('DeleteDialogComponent', () => {
   let component: DeleteDialogComponent;
   let fixture: ComponentFixture<DeleteDialogComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [DeleteDialogComponent]
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DeleteDialogComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(DeleteDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/shared/search-hero/search-hero.component.spec.ts
+++ src/app/shared/search-hero/search-hero.component.spec.ts
@@ -1,15 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SearchHeroComponent } from './search-hero.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('SearchHeroComponent', () => {
   let component: SearchHeroComponent;
   let fixture: ComponentFixture<SearchHeroComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [SearchHeroComponent]
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SearchHeroComponent, NoopAnimationsModule],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(SearchHeroComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
